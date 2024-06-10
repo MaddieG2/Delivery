4f36b66a39d5e3ba19cbf8444328397db060dfb3
medaib mekonnen <medaibmekonnen@gmail.com>
2024-06-10 15:13:51 -0400

    PBI #1: Initial project setup and drivers's APIs

---
+++ Delivery.APIService/DTO/DriverAPIServiceDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delivery.APIService.DTO
+{
+    public class DriverAPIServiceDTO
+    {
+        public int DriverID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string City { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
+
+
+    
---
+++ Delivery/Controllers/DriverController.cs
@@ -0,0 +1,36 @@
+﻿using Delivery.APIService;
+using Delivery.APIService.DTO;
+using Delivery.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Delivery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DriverController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("GetDrivers")]
+        public List<DriverAPIServiceDTO> GetDrivers()
+        {
+            DriverService myDriverSerive = new DriverService(_configuration);
+            List<DriverAPIServiceDTO> myDrivers= myDriverSerive.GetDrivers();   
+            
+            return myDrivers;
+        }
+        
+    }
+
+   
+}
